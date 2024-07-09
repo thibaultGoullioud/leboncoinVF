14cf6e678fcaff4bbf747b807d1dc817ddebf993
thibaultGoullioud <thibault.goullioud@gmail.com>
2024-07-09 16:41:56 +0200

    config plugins timeout

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  // ... Éventuelles autres clefs
   upload: {
     config: {
       provider: "cloudinary",
@@ -8,12 +7,6 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: { timeout: 1000 * 60 * 3 },
-        delete: {},
-      },
     },
   },
-  // ... Éventuelles autres clefs
 });
